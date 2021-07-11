1deb70de08d5220ce45675cd3c7c633ef558a599
prateek-ml <prateekb1912@gmail.com>
2021-07-11 12:06:19 +0530

    Write all page 1 books data into JSON file

--- spiders/books_spider.py
+++ spiders/books_spider.py
@@ -1,17 +1,19 @@
 import scrapy
 
 class BooksSpider(scrapy.Spider):
+    name = 'books_spider'
+
     def start_requests(self):
         url = "http://books.toscrape.com/index.html"
 
         yield scrapy.Request(url, callback=self.parse)
     
-    def parse(self, response, **kwargs):
+    def parse(self, response):
 
-        books = response.css('article.product_pod').getall()
+        books = response.css('article.product_pod')
 
         for b in books:
-            title = b.css("h3 a::attrib('title')")
+            title = b.css("h3 a::attr('title')").get()
             price = float(b.css('p.price_color::text').get()[1:])
 
             yield {
